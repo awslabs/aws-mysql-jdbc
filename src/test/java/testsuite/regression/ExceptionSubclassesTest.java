eef567fae707f8dba1581f874d5249d5f9ea15ea
Sertay Sener <seners@amazon.com>
2021-01-06 15:16:43 -0800

    update modified file license headers

--- src/test/java/testsuite/regression/ExceptionSubclassesTest.java
+++ src/test/java/testsuite/regression/ExceptionSubclassesTest.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2013, 2020, Oracle and/or its affiliates. All rights reserved.
- * Modifications Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright (c) 2002, 2018, Oracle and/or its affiliates. All rights reserved.
+ *
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, version 2.0, as published by the
