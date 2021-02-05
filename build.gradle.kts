1f705aa7c5f7a77bf5af63eba3a9b0946519a113
aaron-congo <aarcongo@amazon.com>
2021-02-05 13:31:43 -0800

    check-fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -214,6 +214,10 @@ tasks.withType<Checkstyle>().configureEach {
     }
 }
 
+tasks.withType<Test>().configureEach {
+    maxParallelForks = 1
+}
+
 dependencies {
     testImplementation("org.apache.commons:commons-dbcp2:2.8.0")
     testImplementation("com.amazonaws:aws-java-sdk-rds:1.11.875")
