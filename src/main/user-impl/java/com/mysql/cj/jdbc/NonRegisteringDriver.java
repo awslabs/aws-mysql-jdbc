1ae4d3e6ec6c1ddc6c0e3a3be404aabd89d0571e
Sertay Sener <seners@amazon.com>
2021-03-17 14:59:45 -0700

    pr feedback

--- src/main/user-impl/java/com/mysql/cj/jdbc/NonRegisteringDriver.java
+++ src/main/user-impl/java/com/mysql/cj/jdbc/NonRegisteringDriver.java
@@ -77,9 +77,6 @@ public class NonRegisteringDriver implements java.sql.Driver {
 
     protected static boolean acceptAwsProtocolOnly = false;
 
-    private static final String MYSQL_PROTOCOL = "jdbc:mysql:";
-    private static final String AWS_PROTOCOL = MYSQL_PROTOCOL + "aws:";
-
     /*
      * Standardizes OS name information to align with other drivers/clients
      * for MySQL connection attributes
@@ -151,7 +148,7 @@ public class NonRegisteringDriver implements java.sql.Driver {
      */
     @Override
     public boolean acceptsURL(String url) throws SQLException {
-        if(acceptAwsProtocolOnly && !url.startsWith(AWS_PROTOCOL)){
+        if(acceptAwsProtocolOnly && !url.startsWith(Type.SINGLE_CONNECTION_AWS.getScheme())){
             return false;
         }
         return (ConnectionUrl.acceptsUrl(url));
