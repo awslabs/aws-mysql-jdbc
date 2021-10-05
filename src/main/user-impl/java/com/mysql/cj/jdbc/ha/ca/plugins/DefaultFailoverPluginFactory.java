202af821421f0c7f50dc9a7be021dffba2922647
Karen Chen <karenc@bitquilltech.com>
2021-10-05 10:12:46 -0700

    chore: Reformat code

--- src/main/user-impl/java/com/mysql/cj/jdbc/ha/ca/plugins/DefaultFailoverPluginFactory.java
+++ src/main/user-impl/java/com/mysql/cj/jdbc/ha/ca/plugins/DefaultFailoverPluginFactory.java
@@ -4,10 +4,13 @@ import com.mysql.cj.conf.HostInfo;
 import com.mysql.cj.conf.PropertySet;
 import com.mysql.cj.log.Log;
 
-public class DefaultFailoverPluginFactory implements IFailoverPluginFactory{
+public class DefaultFailoverPluginFactory implements IFailoverPluginFactory {
   @Override
-  public IFailoverPlugin getInstance(PropertySet propertySet, HostInfo hostInfo, IFailoverPlugin next, Log log) {
-    IFailoverPlugin plugin = new DefaultFailoverPlugin();
+  public IFailoverPlugin getInstance(
+      PropertySet propertySet,
+      HostInfo hostInfo,
+      IFailoverPlugin next, Log log) {
+    final IFailoverPlugin plugin = new DefaultFailoverPlugin();
     plugin.init(propertySet, hostInfo, next, log);
     return plugin;
   }
