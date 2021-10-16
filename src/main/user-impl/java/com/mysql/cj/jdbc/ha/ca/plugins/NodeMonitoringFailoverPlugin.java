4ad5843bab2d14409579440035af10b890e0ae2f
Karen Chen <karenc@bitquilltech.com>
2021-10-15 17:45:14 -0700

    fix: Catch ExecutionException specifically.

--- src/main/user-impl/java/com/mysql/cj/jdbc/ha/ca/plugins/NodeMonitoringFailoverPlugin.java
+++ src/main/user-impl/java/com/mysql/cj/jdbc/ha/ca/plugins/NodeMonitoringFailoverPlugin.java
@@ -34,6 +34,7 @@ import com.mysql.cj.log.Log;
 import org.jboss.util.NullArgumentException;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -182,8 +183,8 @@ public class NodeMonitoringFailoverPlugin implements IFailoverPlugin {
       }
 
       result = executeFuncFuture.get();
-    } catch (Exception ex) {
-      final Throwable throwable = ex.getCause();
+    } catch (ExecutionException exception) {
+      final Throwable throwable = exception.getCause();
       if (throwable instanceof Error) {
         throw (Error) throwable;
       }
