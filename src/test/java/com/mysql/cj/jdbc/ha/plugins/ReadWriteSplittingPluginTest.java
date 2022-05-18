155137fd0d5d1c2b88558f0b7b5160f01e6ef05f
Aaron Congo <aarcongo@amazon.com>
2022-05-18 14:25:17 -0700

    Add ReadWriteSplittingPlugin instantation test

--- src/test/java/com/mysql/cj/jdbc/ha/plugins/ReadWriteSplittingPluginTest.java
+++ src/test/java/com/mysql/cj/jdbc/ha/plugins/ReadWriteSplittingPluginTest.java
@@ -2,16 +2,22 @@ package com.mysql.cj.jdbc.ha.plugins;
 
 import com.mysql.cj.conf.ConnectionUrl;
 import com.mysql.cj.conf.PropertySet;
+import com.mysql.cj.jdbc.JdbcConnection;
 import com.mysql.cj.jdbc.JdbcPropertySetImpl;
 import com.mysql.cj.jdbc.ha.ConnectionProxy;
 import com.mysql.cj.log.Log;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.*;
